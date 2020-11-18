b5e1c5ae4f2c7922a65ad2f8c005e32e9fe0b767
Beyond <1851703552@qq.com>
2020-11-18 10:58:43 +0800

    test

---
+++ src/main/java/HiWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: 张衡
+ * @created: 2020/11/18 10:53
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("hiworld");
+    }
+}
